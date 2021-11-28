782e73da4a32dc67f71a372a425f77d6903cc885
Tobi-Kun <40240050+NZSigourney@users.noreply.github.com>
2021-11-28 14:15:43 +0000

    V2.5 - Fixed Block Events

--- src/NZS/Breaker/EventListener.php
+++ src/NZS/Breaker/EventListener.php
@@ -10,7 +10,7 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\block\BlockBreakEvent;
-
+use pocketmine\level\Level;
 use pocketmine\item\Item;
 use NZS\Breaker\Main;
 
@@ -41,7 +41,9 @@ class EventListener implements Listener
         //Server::getInstance()->getLogger()->critical(Self::getInstance()->getMotd() . "§b Added Point (".$this->plugin->point[$player->getName()]." to Point.yml success");
     }
 
-    
+    public function onBlockSet(BlockBreakEvent $event){
+        
+    }
 
     public function onChat(PlayerChatEvent $ev){
         $p = $ev->getPlayer();
@@ -87,8 +89,11 @@ class EventListener implements Listener
         $PAStone = 274;
         $PAWooden = 270;
 
-        if($p->getInventory()->getItemInHand()->getId() == $PADiamond)
-        {
+        $world = "ai";
+        $level = $p->getLevel()->getName();
+        if($level == $world){
+            $p->getLevel()->setTime(0);
+            if($p->getInventory()->getItemInHand()->getId() == $PADiamond){
 
             if($block->getId() == 56 || $block->getId() == 129) {
                 switch (mt_rand(1, 15)) {
@@ -200,17 +205,14 @@ class EventListener implements Listener
                         $ev->setDrops($drops);
                         $p->sendPopup("§l§f[".Server::getInstance()->getMotd()."§f]§a Added§b ".rand(1,5)."§c ".Item::get(175)->getName()."§a To your Inventory!");
                         break;
+                    }
                 }
-
-
             }
-        }
-
-        if($p->getInventory()->getItemInHand()->getId() == $PAGold || $p->getInventory()->getItemInHand()->getId() == $PAIron || $p->getInventory()->getItemInHand()->getId() == $PAStone || $p->getInventory()->getItemInHand()->getId() == $PAWooden)
-        {
-            if($block->getId() == 56 || $block->getId() == 129)
+            if($p->getInventory()->getItemInHand()->getId() == $PAGold || $p->getInventory()->getItemInHand()->getId() == $PAIron || $p->getInventory()->getItemInHand()->getId() == $PAStone || $p->getInventory()->getItemInHand()->getId() == $PAWooden)
             {
-                switch(mt_rand(1, 12)){
+                if($block->getId() == 56 || $block->getId() == 129)
+                {
+                    switch(mt_rand(1, 12)){
                     case 1:
                         $this->getPlugin()->addPoint($p, 1);
                         $p->sendMessage("§l§f[".Server::getInstance()->getMotd()."§f]§b ".$this->getPlugin()->seePoint($player)." §aAdded to your Vault!");
@@ -259,11 +261,11 @@ class EventListener implements Listener
                         $ev->setDrops($drops);
                         $p->sendPopup("§l§f[".Server::getInstance()->getMotd()."§f]§a Added§b ".rand(1,5)."§c ".Item::get(175)->getName()."§a To your Inventory!");
                         break;
-                }
-            }elseif($block->getId() == 15 || $block->getId() == 75 || $block->getId() == 14 || $block->getId() == 21 || $block->getId() == 16 || $block->getId() == 73)
-            {
-                switch(mt_rand(1, 3)){
-                    case 1:
+                    }
+                }elseif($block->getId() == 15 || $block->getId() == 75 || $block->getId() == 14 || $block->getId() == 21 || $block->getId() == 16 || $block->getId() == 73)
+                {
+                    switch(mt_rand(1, 3)){
+                        case 1:
                         $this->getPlugin()->addPoint($p, 1);
                         $p->sendMessage("§l§f[".Server::getInstance()->getMotd()."§f]§b ".$this->getPlugin()->seePoint($player)." §aAdded to your Vault!");
                         $drops = array();
@@ -271,7 +273,7 @@ class EventListener implements Listener
                         $ev->setDrops($drops);
                         $p->sendPopup("§l§f[".Server::getInstance()->getMotd()."§f]§a Added§b ".rand(1,5)."§c ".Item::get(175)->getName()."§a To your Inventory!");
                         break;
-                    case 2:
+                        case 2:
                         $this->getPlugin()->addExp($p);
                         $p->sendMessage("§l§f[".Server::getInstance()->getMotd()."§f]§b ".$this->getPlugin()->seePoint($player)." §aAdded to your Vault!");
                         $drops = array();
@@ -303,7 +305,81 @@ class EventListener implements Listener
                         $ev->setDrops($drops);
                         $p->sendPopup("§l§f[".Server::getInstance()->getMotd()."§f]§a Added§b ".rand(1,5)."§c ".Item::get(175)->getName()."§a To your Inventory!");
                         break;
+                    }
                 }
+            }
+        }else{
+            $p->sendMessage("Qua LAVA mà đào đi đũy ngựa!");
+        }
+
+        
+        
+        # Random Ore
+
+        /**$block = $event->getBlock();
+        $water = false;
+        $lava = false;
+        for ($i = 2; $i <= 5; $i++) {
+            $nearBlock = $block->getSide($i);
+            if ($nearBlock instanceof Water) {
+                $water = true;
+            } else if ($nearBlock instanceof Lava || $nearBlock instanceof Fence) {
+                $lava = true;
+            }
+            if ($water && $lava) {
+                $id = mt_rand(1, 65);
+                switch ($id) {
+                    case 2;
+                        $newBlock = new Iron();
+                        break;
+                    case 4;
+                        $newBlock = new Gold();
+                        break;
+                    case 6;
+                        $newBlock = new Emerald();
+                        break;
+                    case 8;
+                        $newBlock = new Coal();
+                        break;
+                    case 10;
+                        $newBlock = new Redstone();
+                        break;
+                    case 12;
+                        $newBlock = new Diamond();
+                        break;
+					case 14;
+                        $newBlock = new Lapis();
+                        break;	
+						case 16;
+                        $newBlock = new Block(15);
+                        break;	
+						case 19;
+                        $newBlock = new Block(21);
+					
+                        break;	
+						case 21;
+                        $newBlock = new Block(14);
+                        break;	
+							case 27;
+                        $newBlock = new Block(15);
+                        break;	
+							case 29;
+                        $newBlock = new Block(56);
+                        break;
+                        case 25;
+                        $newBlock = new Block(73);
+                        break;	
+							case 28;
+                        $newBlock = new Block(129);
+                        break;	
+							case 32;
+                        $newBlock = new Block(153);
+                        break;	
+                    default:
+                        $newBlock = new Cobblestone();
+                }
+                $block->getLevel()->setBlock($block, $newBlock, true, false);
+                return;
             }
         }
 
